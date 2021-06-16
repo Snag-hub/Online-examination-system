a646446437e59f34da25b47ea4108150b7962ae1
Syed Nadeem Hussain <78342383+Snag-hub@users.noreply.github.com>
2021-06-16 11:34:18 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+<Center><h2>About Us</h2>
+
+<h3>I am Working on this Page</h3></Center>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos facere quasi consectetur aliquam a dolorem, perspiciatis quidem, vero incidunt similique, rerum iure? Magnam vel magni praesentium provident in earum nisi.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolore vitae! Explicabo suscipit, iure, quo repudiandae nostrum, autem at error molestiae necessitatibus rerum distinctio laudantium assumenda! Eligendi molestiae dolore rem blanditiis, voluptas molestias harum adipisci porro perferendis officiis mollitia ex repudiandae illum repellat quasi ratione odio iste. In dignissimos pariatur modi recusandae ab, obcaecati incidunt dolor, cumque optio debitis corporis quibusdam asperiores eos, architecto minima provident iste voluptas hic accusamus est. Explicabo, maxime! Culpa repudiandae tenetur, hic molestias repellendus exercitationem ad eligendi pariatur delectus tempore optio error in obcaecati est vitae libero! Rerum ipsam nam illum iure aut rem. Quibusdam.</p>
+
+    
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,64 @@
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4 footer">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase" style="color:aliceblue">Online Examination/Quiz</h5>
+        <p style="color:aliceblue">This is Online Quiz Site.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase" style="color:aliceblue">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Instagram</a>
+          </li>
+          <li>
+            <a href="#!">Facebook</a>
+          </li>
+          <li>
+            <a href="#!">Twitter</a>
+          </li>
+          <li>
+            <a href="#!">Github</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3" style="color:aliceblue">© 2021 Copyright:
+    <a href="https://www.techiesnag.blogspot.com"> Syed Nadeem Hussain</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
